a9246be79d80694adee37e0a88d4c31f731af1df
jaydanflo <160744639+jaydanflo@users.noreply.github.com>
2025-01-29 11:51:34 -0500

    Update Car.java

--- src/main/java/Car.java
+++ src/main/java/Car.java
@@ -1,16 +1,30 @@
 public class Car extends Vehicle {
 
+    String brand;
+
+    public Car (int numberOfWheels, String color, float engineSize, String fuelType, String brand) {
+        super(numberOfWheels, color, engineSize, fuelType)
+        this.brand = brand;
+        
+    }
+    
     public String getBrand() {
-        String brand;
-        return
+        return brand;
     }
 
-    public void setbrand(){
-
-
+    public void setbrand(String brand;){
+        this.brand = brand;
     }
 
     public void honk() {
+        System.out.println("Honk, honk!");
+    }
 
+    public void displayInfo() {
+        System.out.println("Brand: " + brand);
+        System.out.println("Number of Wheels: " + getNumberOfWheels());
+        System.out.println("Color: " + getColor());
+        System.out.println("Engine Size: " + getEngineSize());
+        System.out.println("Fuel Type: " + getFuelType());
     }
 }

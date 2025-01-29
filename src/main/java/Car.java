9afe632d9f5ae882099c5d5385833c76e06538ea
jaydanflo <jaydanf2@gmail.com>
2025-01-29 10:23:21 -0500

    commit

---
+++ src/main/java/Car.java
@@ -0,0 +1,16 @@
+public class Car extends Vehicle {
+
+    public String getBrand() {
+        String brand;
+        return
+    }
+
+    public void setbrand(){
+
+
+    }
+
+    public void honk() {
+
+    }
+}
---
+++ src/main/java/Vehicle.java
@@ -0,0 +1,44 @@
+public class Vehicle {
+
+    public int getNumberOfWheels() {
+        int numberOfWheels;
+
+        return 0;
+    }
+    public void setNumberofWheels() {
+
+
+    }
+
+    public String getColor() {
+        String color;
+
+        return "";
+    }
+
+    public void setColor(){
+
+    }
+
+    public void getEngineSize() {
+        float engineSize;
+
+        return ;
+    }
+
+    public void setEngineSize() {
+
+
+    }
+
+    public String getFuelType() {
+        String fuelType;
+
+        return "";
+    }
+
+    public void setFuelType() {
+
+
+    }
+}
